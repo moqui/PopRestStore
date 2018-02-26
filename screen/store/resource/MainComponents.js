c6791feb4d2d19db11a96272f59b24c233fafce7
David E Jones <dej@dejc.com>
2018-02-25 22:17:01 -0800

    Experiment with x-template for vue component template separation

--- screen/store/resource/MainComponents.js
+++ screen/store/resource/MainComponents.js
@@ -1,6 +1,6 @@
 /* This software is in the public domain under CC0 1.0 Universal plus a Grant of Patent License. */
 
 const HomeComponent = {
-    template: '<h2>Home Page</h2>'
+    template: '#HomeTemplate'
 };
 
