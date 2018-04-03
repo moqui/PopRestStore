f70a43a2604edbef1a0626268dba2dddc4a14d08
Anderson <andersonur@hotmail.com>
2018-04-03 18:16:06 -0500

    changes in the styles

--- screen/store/components/pages/CheckOutPage/CheckOutPage.js
+++ screen/store/components/pages/CheckOutPage/CheckOutPage.js
@@ -11,6 +11,7 @@ var CheckOutPage = {
       addressOption: "",
       paymentOption: "",
       paymentId: {},
+      isShippingMethod:false,
       listShippingOptions: [],
       axiosConfig: {
         headers: {
